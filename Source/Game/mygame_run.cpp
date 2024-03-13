9bb548f6ce52e0f5f42c470a2da6737d0b3cf7fd
biabib <kevin20040503@gmail.com>
2024-03-13 17:35:40 +0800

    2

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -27,17 +27,98 @@ void CGameStateRun::OnBeginState()
 
 void CGameStateRun::OnMove()							// 移動遊戲元素
 {
-	
 }
 
 void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 {
-	
+	background.LoadBitmapByString({ 
+		"resources/phase11_background.bmp", 
+		"resources/phase12_background.bmp", 
+		"resources/phase21_background.bmp", 
+		"resources/phase22_background.bmp", 
+		"resources/phase31_background.bmp", 
+		"resources/phase32_background.bmp",
+		"resources/phase41_background.bmp",
+		"resources/phase42_background.bmp",
+		"resources/phase51_background.bmp",
+		"resources/phase52_background.bmp",
+		"resources/phase61_background.bmp",
+		"resources/phase62_background.bmp",
+	});
+	background.SetTopLeft(0, 0);
+
+	character.LoadBitmapByString({ "resources/gray.bmp" });
+	character.SetTopLeft(150, 265);
+
+	chest_and_key.LoadBitmapByString({ "resources/chest.bmp", "resources/chest_ignore.bmp" }, RGB(255, 255, 255));
+	chest_and_key.SetTopLeft(150, 430);
+
+	bee.LoadBitmapByString({ "resources/bee_1.bmp", "resources/bee_2.bmp" });
+	bee.SetTopLeft(462, 265);
+
+	ball.LoadBitmapByString({ "resources/ball-3.bmp", "resources/ball-2.bmp", "resources/ball-1.bmp", "resources/ball-ok.bmp" });
+	ball.SetTopLeft(150, 430);
+
+	for (int i = 0; i < 3; i++) {
+		door[i].LoadBitmapByString({ "resources/door_close.bmp", "resources/door_open.bmp" }, RGB(255, 255, 255));
+		door[i].SetTopLeft(462 - 100 * i, 265);
+	}
 }
 
 void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-	
+	if (nChar == VK_RETURN) {
+		if (phase == 1) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_1();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+			}
+		} else if (phase == 2) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_2();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+			}
+		}else if (phase == 3) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_3();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+			}
+		}else if (phase == 4) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_4();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+			}
+		}else if (phase == 5) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_5();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+			}
+		}else if (phase == 6) {
+			if (sub_phase == 1) {
+				sub_phase += validate_phase_6();
+			}
+			else if (sub_phase == 2) {
+				sub_phase = 1;
+				phase += 1;
+				GotoGameState(GAME_STATE_OVER);
+			}
+		}
+	}
 }
 
 void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
@@ -67,4 +148,65 @@ void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
 
 void CGameStateRun::OnShow()
 {
+	show_image_by_phase();
+	show_text_by_phase();
+}
+
+void CGameStateRun::show_image_by_phase() {
+	if (phase <= 6) {
+		background.SelectShowBitmap((phase - 1) * 2 + (sub_phase - 1));
+		background.ShowBitmap();
+		character.ShowBitmap();
+		if (phase == 3 && sub_phase == 1) {
+			chest_and_key.ShowBitmap();
+		}
+		if (phase == 4 && sub_phase == 1) {
+			bee.ShowBitmap();
+		}
+		if (phase == 5 && sub_phase == 1) {
+			for (int i = 0; i < 3; i++) {
+				door[i].ShowBitmap();
+			}
+		}
+		if (phase == 6 && sub_phase == 1) {
+			ball.ShowBitmap();
+		}
+	}
+}
+
+void CGameStateRun::show_text_by_phase() {
+	
+}
+
+bool CGameStateRun::validate_phase_1() {
+	return character.GetImageFilename() == "resources/giraffe.bmp";
+}
+
+bool CGameStateRun::validate_phase_2() {
+	return character.Top() > 204 && character.Top() < 325 && character.Left() > 339 && character.Left() < 459;
+}
+
+bool CGameStateRun::validate_phase_3() {
+	return (
+		character.Top() + character.Height() >= chest_and_key.Top()
+		&& character.Left() + character.Width() >= chest_and_key.Left()
+		&& chest_and_key.GetSelectShowBitmap() == 1
+		&& chest_and_key.GetFilterColor() == RGB(255, 255, 255)
+	);
+}
+
+bool CGameStateRun::validate_phase_4() {
+	return bee.IsAnimation() && bee.GetMovingBitmapFrame() == 2;
+}
+
+bool CGameStateRun::validate_phase_5() {
+	bool check_all_door_is_open = true;
+	for (int i = 0; i < 3; i++) {
+		check_all_door_is_open &= (door[i].GetSelectShowBitmap() == 1);
+	}
+	return check_all_door_is_open;
+}
+
+bool CGameStateRun::validate_phase_6() {
+	return ball.IsAnimationDone() && !ball.IsAnimation();
 }
